2ac3c9633072488eb35e056d1cb0d3942b4446b4
EssGhost <yolotlperez8@gmail.com>
2024-09-30 12:58:32 -0600

    to-do list iniciando

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './index.css';
+import { FaPlus, FaTrash, FaCheck } from 'react-icons/fa'; // Importamos los iconos de Font Awesome
 
-function App() {
+function ToDoList() {
+  const [tareas, setTareas] = useState([
+    {
+      id: 1,
+      texto: 'Comprar leche',
+      completada: false
+    },
+    {
+      id: 2,
+      texto: 'Hacer ejercicio',
+      completada: false
+    },
+  ]);
+
+  const [nuevaTarea, setNuevaTarea] = useState({
+    texto: '',
+    completada: false
+  });
+
+  const agregarTarea = (e) => {
+    e.preventDefault();
+    const nuevaTareaId = tareas.length + 1;
+    const nuevaTareaCompleta = { id: nuevaTareaId, ...nuevaTarea };
+    setTareas([...tareas, nuevaTareaCompleta]);
+    setNuevaTarea({ texto: '', completada: false });
+  };
+
+  const eliminarTarea = (id) => {
+    setTareas(tareas.filter((tarea) => tarea.id !== id));
+  };
+
+  const toggleCompletada = (id) => {
+    setTareas(tareas.map((tarea) => {
+      if (tarea.id === id) {
+        return { ...tarea, completada: !tarea.completada };
+      }
+      return tarea;
+    }));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="to-do-list">
+      <h1>Lista de tareas</h1>
+      <ul>
+        {tareas.map((tarea) => (
+          <li key={tarea.id} className={tarea.completada ? 'completada' : ''}>
+            {/* <input type="checkbox" checked={tarea.completada} onChange={() => toggleCompletada(tarea.id)} /> */}
+            <span>{tarea.texto}</span>
+            <button className="eliminar" onClick={() => eliminarTarea(tarea.id)}>
+              <FaTrash /> 
+            </button>
+            <button className="completar" onClick={() => toggleCompletada(tarea.id)}>
+              <FaCheck />
+            </button>
+          </li>
+        ))}
+      </ul>
+      <form onSubmit={agregarTarea}>
+        <h2>Agregar tarea</h2>
+        <label>
+          Tarea:
+          <input type="text" value={nuevaTarea.texto} onChange={(e) => setNuevaTarea({ ...nuevaTarea, texto: e.target.value })} />
+        </label>
+        <button type="submit">
+          <FaPlus /> 
+        </button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default ToDoList;
