945e09afdb3dc16e6ff651ea0de43c006cad2be3
EssGhost <yolotlperez8@gmail.com>
2024-10-14 00:46:47 -0600

    5

--- src/App.js
+++ src/App.js
@@ -5,6 +5,46 @@ import { FaPlus, FaTrash, FaCheck } from 'react-icons/fa'; // Importamos los ico
 import InstallButton from './InstallButton';
 
 function ToDoList() {
+
+  const [deferredPrompt, setDeferredPrompt] = useState(null);
+  const [showInstallButton, setShowInstallButton] = useState(false);
+
+  useEffect(() => {
+    const handleBeforeInstallPrompt = (e) => {
+      // Prevenir el prompt automático
+      e.preventDefault();
+      // Guardar el evento para lanzarlo manualmente más tarde
+      setDeferredPrompt(e);
+      // Mostrar un botón o cualquier UI para instalar
+      setShowInstallButton(true);
+    };
+
+    // Escuchar el evento 'beforeinstallprompt'
+    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+
+    return () => {
+      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
+    };
+  }, []);
+
+  const handleInstallClick = () => {
+    if (deferredPrompt) {
+      // Lanzar el prompt de instalación
+      deferredPrompt.prompt();
+
+      // Ver qué decide el usuario
+      deferredPrompt.userChoice.then((choiceResult) => {
+        if (choiceResult.outcome === 'accepted') {
+          console.log('El usuario aceptó la instalación');
+        } else {
+          console.log('El usuario rechazó la instalación');
+        }
+        setDeferredPrompt(null);  // Resetear el deferredPrompt
+        setShowInstallButton(false);  // Ocultar el botón
+      });
+    }
+  };
+
   const [tareas, setTareas] = useState([
     {
       id: 1,
@@ -77,11 +117,13 @@ function ToDoList() {
         </button>
       </form>
       
-      <button type="submit">
-        Instalar mi PWA
-      <InstallButton />
+      {/* Mostrar botón solo si el evento beforeinstallprompt fue capturado */}
+      {showInstallButton && (
+        <button onClick={handleInstallClick}>
+          Instalar PWA
         </button>
-      
+      )}
+
     </div>
   );
 }
