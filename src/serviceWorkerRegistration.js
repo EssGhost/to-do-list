4f8c3f24b2b5203f02501a06cf77e3a87efc95bc
EssGhost <yolotlperez8@gmail.com>
2024-10-14 00:23:06 -0600

    4.6

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,10 +1,8 @@
-// src/serviceWorkerRegistration.js
+
 
 const isLocalhost = Boolean(
     window.location.hostname === 'localhost' ||
-      // [::1] is the IPv6 localhost address.
       window.location.hostname === '[::1]' ||
-      // 127.0.0.0/8 are considered localhost for IPv4.
       window.location.hostname.match(
         /^127(?:\.\d+){0,2}\.\d+$/
       )
